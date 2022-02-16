e48badda184b51139de58b5049b0e7a3a35a1e73
Vince-22 <vincent.poirier2245@gmail.com>
2022-02-16 16:36:15 +0100

    tout

--- myjs.js
+++ myjs.js
@@ -1,7 +1,2 @@
-var smoothScroll = new ScrollToAnchor();
-var smoothScroll = new ScrollToAnchor({
-    duration: 1000
-});
-var smoothScroll = new ScrollToAnchor({
-    offset: 70
-});
+
+        AOS.init();

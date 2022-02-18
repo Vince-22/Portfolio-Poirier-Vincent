6397449a730f418d6a0f32792de4d70a6ea150b4
Vince-22 <vincent.poirier2245@gmail.com>
2022-02-18 12:01:13 +0100

    narbarr

--- myjs.js
+++ myjs.js
@@ -1,5 +1,19 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
 AOS.init();
 $(".image").click(function(){
     $(".image").removeClass("active");
     $(this).addClass("active")
 })
+
+
+navToggle.addEventListener('click', ()=> {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+}) 
